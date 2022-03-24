6ddd1ae9a3cdb7f238855e5ac7288f6d578d1841
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-03-24 16:02:05 +0200

    Добавьте файлы проекта.

---
+++ aditionalTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace aditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory = new DirectoryInfo(@"C:\TESTDIR");
+            directory.Create();
+            if (directory.Exists)
+            {
+                for (int i = 0; i < 100; i++)
+                    directory.CreateSubdirectory("Folder_" + i);
+            }
+            try
+            {
+                directory.Delete(true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
