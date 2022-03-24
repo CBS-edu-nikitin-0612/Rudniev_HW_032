aa5a8102f5f2fe06f6dcd18a7e54d70755a1f1d4
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-03-24 16:39:34 +0200

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new FileInfo(@"D:\test.txt");
+            
+            StreamWriter streamWriter = file.CreateText();
+            streamWriter.Write("hello world!");
+            streamWriter.Close();
+            
+            StreamReader streamReader = file.OpenText();
+            Console.WriteLine(streamReader.ReadToEnd());
+            streamReader.Close();
+        }
+    }
+}
